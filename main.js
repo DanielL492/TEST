d14546bd1abe696187aa8944b3d9307c2b82f03b
DanielL492 <144807703+DanielL492@users.noreply.github.com>
2023-12-01 09:10:43 -0500

    df

--- main.js
+++ main.js
@@ -16,13 +16,10 @@ const URL = `https://api.quotable.io/random`;
 async function getData(URL) {
     try { //pretend forEach for each card
         const response = await fetch(URL);
-        console.log(response);
         const data = await response.json();
         console.log(data);
-        document.querySelector("h1").textContent = data.content;
-        document.querySelector("h2").textContent = data.author;
-    } catch (error) {
-
-    }
+        //document.querySelector("h1").textContent = data.content;
+        //document.querySelector("h2").textContent = data.author;
+    } catch (error) {}
 }
 getData(URL);
